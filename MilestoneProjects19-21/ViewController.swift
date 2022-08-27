ea5d3c62a51c5414b98961890a5a475de3f77393
Filip Cernov <philipchernov95@gmail.com>
2022-08-27 17:01:11 -0400

    1. Fix bug where JSON was not loaded properly 2. General code optimisation

--- MilestoneProjects19-21/ViewController.swift
+++ MilestoneProjects19-21/ViewController.swift
@@ -36,6 +36,50 @@ class NotesTableViewController: UIViewController {
         loadJSON()
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showNote", sender: self)
+    }
+    
+
+    @IBAction func addNewNote(_ sender: UIButton) {
+        let note = Note(title: "New note", text: "")
+        notes.append(note)
+        let requestBody = Notes(notes: notes)
+        let jsonString = convertObjectIntoJSONString(requestBody: requestBody)
+        saveJSONData(jsonString)
+        tableView.reloadData()
+    }
+    
+    func convertObjectIntoJSONString(requestBody : Notes) -> String? {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        do {
+            let result = try encoder.encode(requestBody)
+            if let jsonString = String(data: result, encoding: .utf8) {
+                // JSON STRING
+                return jsonString
+            }
+        } catch {
+            print("Your parsing sucks \(error)")
+        }
+        return nil
+    }
+    
+    
+    func saveJSONData(_ jsonString: String?) {
+        guard let jsonString = jsonString else { return }
+        if let jsonData = jsonString.data(using: .utf8),
+            let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                             in: .userDomainMask).first {
+            let pathWithFileName = documentDirectory.appendingPathComponent("notes")
+            do {
+                try jsonData.write(to: pathWithFileName)
+            } catch {
+                // handle error
+            }
+        }
+    }
+    
     func loadJSON() {
         DispatchQueue.global().async {
             if let documentDirectory = FileManager.default.urls(for: .documentDirectory,
@@ -46,46 +90,12 @@ class NotesTableViewController: UIViewController {
                     let decoder = JSONDecoder()
                     let jsonData = try decoder.decode(Notes.self, from: data)
                     self.notes = jsonData.notes
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
                 } catch {
                     print("error:\(error)")
                 }
-            }
-        }
-    }
-    
-    @IBAction func addNewNote(_ sender: UIButton) {
-        let note = Note(title: "New note", text: "")
-        notes.append(note)
-        let requestBody = Notes(notes: notes)
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        do {
-            let result = try encoder.encode(requestBody)
-            if let jsonString = String(data: result, encoding: .utf8){
-                // JSON STRING
-                print("JSON \(jsonString)")
-                saveJSONData(jsonString)
-            }
-        } catch {
-            print("Your parsing sucks \(error)")
-        }
-        tableView.reloadData()
-    }
-    
-    func convertObjectIntoJSONString(requestBody : Notes) -> String {
-        
-    }
-    
-    
-    func saveJSONData(_ jsonString: String) {
-        if let jsonData = jsonString.data(using: .utf8),
-            let documentDirectory = FileManager.default.urls(for: .documentDirectory,
-                                                             in: .userDomainMask).first {
-            let pathWithFileName = documentDirectory.appendingPathComponent("notes")
-            do {
-                try jsonData.write(to: pathWithFileName)
-            } catch {
-                // handle error
             }
         }
     }
