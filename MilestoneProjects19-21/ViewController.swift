64ba76313ed2b8200e39add4caa7bebe062f7877
Filip Cernov <philipchernov95@gmail.com>
2022-08-27 16:24:29 -0400

    Add function that seems to create swift string

--- MilestoneProjects19-21/ViewController.swift
+++ MilestoneProjects19-21/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class Notes {
-    var notes = [Note]()
+class Notes: Codable {
+    var notes: [Note]
+    init (notes: [Note]) {
+        self.notes = notes
+    }
 }
 
-class Note {
-    var title = String()
-    var text = String()
+class Note: Codable {
+    var title: String
+    var text: String
+    init(title: String, text: String) {
+        self.title = title
+        self.text = text
+    }
 }
 
 class NotesTableViewController: UIViewController {
@@ -26,17 +33,43 @@ class NotesTableViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        loadJSON()
+    }
+    
+    func loadJSON() {
+        DispatchQueue.global().async {
+            if let url = Bundle.main.url(forResource: "notes", withExtension: "json") {
+                   do {
+                       let data = try Data(contentsOf: url)
+                       let decoder = JSONDecoder()
+                       let jsonData = try decoder.decode(Notes.self, from: data)
+                       self.notes = jsonData.notes
+                   } catch {
+                       print("error:\(error)")
+                   }
+               }
+        }
     }
     
     @IBAction func addNewNote(_ sender: UIButton) {
-        let note = Note()
-        note.title = "New note"
-        note.text = ""
+        let note = Note(title: "New note", text: "")
         notes.append(note)
+        let requestBody = Notes(notes: notes)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        do {
+            let result = try encoder.encode(requestBody)
+            // RESULT IS NOW JSON-LIKE DATA OBJECT
+            if let jsonString = String(data: result, encoding: .utf8){
+                // JSON STRING
+                print("JSON \(jsonString)")
+            }
+        } catch {
+            print("Your parsing sucks \(error)")
+        }
         tableView.reloadData()
     }
     
-
 }
 
 extension NotesTableViewController : UITableViewDelegate, UITableViewDataSource {
